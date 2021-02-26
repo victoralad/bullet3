83f65217d49e36b0d2ee659c33757d0e31c4d843
Victor-Aladele <valad@gatech.edu>
2021-02-26 17:49:27 -0500

    Closer to achieving inverse dynamic control.

--- examples/pybullet/examples/va_thesis/va_sim.py
+++ examples/pybullet/examples/va_thesis/va_sim.py
@@ -67,45 +67,60 @@ class ObjDyn:
     if (self.useSimulation and self.useRealTimeSimulation == 0):
       p.stepSimulation()
 
-    pos = [0.2 * math.cos(self.t), 0.2 * math.sin(self.t), 0.4]
+    desired_pos = [0.2 * math.cos(self.t), 0.2 * math.sin(self.t), 0.4]
     #end effector points down, not up (when orientation is used)
-    orn_euler = [0, -math.pi, 0]
-    orn = p.getQuaternionFromEuler(orn_euler)
+    desired_orn_euler = [0, -math.pi, 0]
 
-    # jacobian = p.calculateJacobian(self.kukaId, self.kukaEndEffectorIndex, )
+    # State of end effector.
+    ee_state = p.getLinkState(self.kukaId, self.kukaEndEffectorIndex, computeLinkVelocity=1, computeForwardKinematics=1)
 
-    jointPoses = p.calculateInverseKinematics(self.kukaId,
-                                              self.kukaEndEffectorIndex,
-                                              pos,
-                                              orn,
-                                              jointDamping=self.jd)
+    link_trn, link_rot, com_trn, com_rot, frame_pos, frame_rot, link_vt, link_vr = ee_state
+
+    # Get the joint and link state directly from Bullet.
+    joints_pos, joints_vel, joints_torq = self.getJointStates(self.kukaId)
+
+    # Get the Jacobians for the CoM of the end-effector link.
+    # Note that in this example com_rot = identity, and we would need to use com_rot.T * com_trn.
+    # The localPosition is always defined in terms of the link frame coordinates.
     
+    zero_vec = [0.0] * len(joints_pos)
+    jac_t, jac_r = p.calculateJacobian(self.kukaId, self.kukaEndEffectorIndex, frame_pos, joints_pos, zero_vec, zero_vec)
+    
+    jac = [jac_t, jac_r]
+    ee_pose = [frame_pos, frame_rot]
+    desired_ee_pose = [desired_pos, desired_orn_euler]
+    ee_vel = [link_vt, link_vr]
+    desired_ee_vel = [0.0] * len(ee_vel)
+
+    ee_pose_error = desired_ee_pose - ee_pose
+    ee_vel_error = desired_ee_vel - ee_vel
+
+    desired_ee_wrench = self.Kp * ee_pose_error + self.Kv * ee_vel_error
+
+    nonlinear_forces = p.calculateInverseDynamics(self.kukaId, joints_pos, joints_vel, zero_vec)
+    desired_joint_torques = jac.transpose() * desired_ee_wrench + nonlinear_forces
+
     if (self.useSimulation):
       for i in range(self.numJoints):
-        p.setJointMotorControl2(bodyIndex=self.kukaId,
-                                jointIndex=i,
-                                controlMode=p.POSITION_CONTROL,
-                                targetPosition=jointPoses[i],
-                                targetVelocity=0,
-                                force=500,
-                                positionGain=0.03,
-                                velocityGain=1)
+        p.setJointMotorControlArray(bodyIndex=self.kukaId,
+                                jointIndices=range(self.numJoints),
+                                controlMode=p.TORQUE_CONTROL,
+                                force=desired_joint_torques)
     else:
       #reset the joint state (ignoring all dynamics, not recommended to use during simulation)
       for i in range(self.numJoints):
         p.resetJointState(self.kukaId, i, jointPoses[i])
     
-    ls = p.getLinkState(self.kukaId, self.kukaEndEffectorIndex)
 
-    if (self.hasPrevPose):
-      #self.trailDuration is duration (in seconds) after debug lines will be removed automatically
-      #use 0 for no-removal
-      trailDuration = 15
-      p.addUserDebugLine(self.prevPose, pos, [0, 0, 0.3], 1, trailDuration)
-      p.addUserDebugLine(self.prevPose1, ls[4], [1, 0, 0], 1, trailDuration)
-    self.prevPose = pos
-    self.prevPose1 = ls[4]
-    self.hasPrevPose = 1
+    # if (self.hasPrevPose):
+    #   #self.trailDuration is duration (in seconds) after debug lines will be removed automatically
+    #   #use 0 for no-removal
+    #   trailDuration = 15
+    #   p.addUserDebugLine(self.prevPose, pos, [0, 0, 0.3], 1, trailDuration)
+    #   p.addUserDebugLine(self.prevPose1, ls[4], [1, 0, 0], 1, trailDuration)
+    # self.prevPose = pos
+    # self.prevPose1 = ls[4]
+    # self.hasPrevPose = 1
 
     keys = p.getKeyboardEvents()
     close_cmd = ord('c')
@@ -117,6 +132,22 @@ class ObjDyn:
       # print("Open gripper")
       self.gripper(0.085)
 
+  def getJointStates(self, kukaId):
+    joint_states = p.getJointStates(kukaId, range(self.numJoints))
+    joint_positions = [state[0] for state in joint_states]
+    joint_velocities = [state[1] for state in joint_states]
+    joint_torques = [state[3] for state in joint_states]
+    return joint_positions, joint_velocities, joint_torques
+  
+  def multiplyJacobian(self, kukaId, jacobian, vector):
+    result = [0.0, 0.0, 0.0]
+    i = 0
+    for c in range(len(vector)):
+      if p.getJointInfo(kukaId, c)[3] > -1:
+        for r in range(3):
+          result[r] += jacobian[r][i] * vector[c]
+        i += 1
+    return result
 
   def gripper(self, gripper_opening_length, mode=p.POSITION_CONTROL):
         '''
