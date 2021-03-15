dd364c68ed12bad92db658b5353325346f097aad
Victor-Aladele <valad@gatech.edu>
2021-03-15 14:43:43 -0400

    Added a script to call training.

---
+++ examples/pybullet/gym/pybullet_envs/va_gym/va_train.py
@@ -0,0 +1,17 @@
+import gym
+
+from stable_baselines.ddpg.policies import FeedForwardPolicy
+from stable_baselines import DDPG
+
+# Custom MLP policy of two layers of size 16 each
+class CustomDDPGPolicy(FeedForwardPolicy):
+    def __init__(self, *args, **kwargs):
+        super(CustomDDPGPolicy, self).__init__(*args, **kwargs,
+                                           layers=[16, 16],
+                                           layer_norm=False,
+                                           feature_extraction="mlp")
+
+
+model = DDPG('CnnPolicy', 'CoopEnv-v0', verbose=1)
+# Train the agent
+model.learn(total_timesteps=1000)
