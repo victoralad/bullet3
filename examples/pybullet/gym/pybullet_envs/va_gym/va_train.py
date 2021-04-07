b69b9642f8c7e4ef49312124b422eb1a4664c789
Victor-Aladele <valad@gatech.edu>
2021-04-07 15:49:30 -0400

    Attempting to use the PPO2 algorithm.

--- examples/pybullet/gym/pybullet_envs/va_gym/va_train.py
+++ examples/pybullet/gym/pybullet_envs/va_gym/va_train.py
@@ -2,7 +2,7 @@ import gym
 import numpy as np
 import pickle
 
-from stable_baselines import DDPG
+from stable_baselines import DDPG, PPO2
 from stable_baselines.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise, AdaptiveParamNoiseSpec
 
 env = gym.make('CoopEnv-v0')
@@ -10,15 +10,16 @@ env = gym.make('CoopEnv-v0')
 n_actions = env.action_space.shape[-1]
 action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=float(0.5) * np.ones(n_actions))
 
-model = DDPG('MlpPolicy', env, verbose=1, action_noise=action_noise)
+# model = DDPG('MlpPolicy', env, verbose=1, action_noise=action_noise)
 # model = DDPG('MlpPolicy', env, verbose=1)
+model = PPO2('MlpPolicy', env, verbose=1)
 
 # Train the agent
-model.learn(total_timesteps=3000)
+model.learn(total_timesteps=1000)
 print("")
 print("---------- Done training -----------------")
 
-model.save("ddpg_coop_manip")
+model.save("ppo_coop_manip")
 
 with open('data/reward.data', 'wb') as filehandle:
     pickle.dump(env.reward_data, filehandle)
