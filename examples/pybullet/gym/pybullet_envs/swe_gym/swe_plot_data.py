48b44cd3fe68fcdd37e59f68d16bbea1f9b8d108
Victor-Aladele <valad@gatech.edu>
2021-11-01 17:26:58 +0100

    Plotting of rewards introduced. Policy network modified.

---
+++ examples/pybullet/gym/pybullet_envs/swe_gym/swe_plot_data.py
@@ -0,0 +1,28 @@
+import pickle
+import matplotlib.pyplot as plt
+
+with open('data/reward.data', 'rb') as filehandle:
+    # read the data as binary data stream
+    reward_data = pickle.load(filehandle)
+
+# Aesthetic artifact
+reward_data[1][0] = min(reward_data[1])
+reward_data[1][1] = reward_data[1][0]
+
+print(reward_data)
+
+plt.plot(reward_data[0], reward_data[1])
+# naming the x axis
+plt.xlabel('num-episodes')
+# naming the y axis
+plt.ylabel('cummulative-reward')
+  
+# giving a title to my graph
+plt.title('Plot of cumulative rewards per episode')
+
+# Saving the figure.
+plot_num = 2
+plt.savefig("data/ppo2_rewards_plot_{}.jpg".format(plot_num))
+
+plt.show()
+
