5253ab3caaefc0438301ef4239c147285e28a502
Victor-Aladele <valad@gatech.edu>
2021-11-19 12:46:58 +0100

    Added plot from prior branch (plot_19 with delta2).

--- examples/pybullet/gym/pybullet_envs/swe_gym/swe_plot_data.py
+++ examples/pybullet/gym/pybullet_envs/swe_gym/swe_plot_data.py
@@ -51,10 +51,10 @@ plt.xlabel('num-time-steps')
 plt.ylabel('avg-obj-pose-error-norm')
   
 # giving a title to my graph
-plt.title('Plot of average object pose error norm (No RL) \n Number of steps = {}, overall avg obj pose error norm = {:.3f}'.format(obj_pose_error_data[0][-1], obj_pose_error_data[1][-1]))
+plt.title('Plot of average object pose error norm \n Number of steps = {}, overall avg obj pose error norm = {:.3f}'.format(obj_pose_error_data[0][-1], obj_pose_error_data[1][-1]))
 
 # Saving the figure.
 plot_num = 19
-plt.savefig("data/ppo2_rewards_plot_{}_No_RL.jpg".format(plot_num))
+plt.savefig("data/ppo2_rewards_plot_{}.jpg".format(plot_num))
 
 plt.show()
