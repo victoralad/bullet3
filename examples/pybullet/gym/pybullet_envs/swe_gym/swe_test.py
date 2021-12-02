d21320881ca124fda84aac87e5b4d66fcdf160bc
Victor-Aladele <valad@gatech.edu>
2021-12-02 18:50:05 +0100

    Plot performance data of policy during testing.

--- examples/pybullet/gym/pybullet_envs/swe_gym/swe_test.py
+++ examples/pybullet/gym/pybullet_envs/swe_gym/swe_test.py
@@ -1,4 +1,5 @@
 import gym
+import pickle
 
 from stable_baselines.ddpg.policies import FeedForwardPolicy
 from stable_baselines import DDPG, PPO2
@@ -7,10 +8,15 @@ model = PPO2.load("ppo_coop_manip")
 
 env = gym.make('CoopEnv-v0')
 obs = env.reset()
-while True:
+max_test_steps = 1000
+while env.time_step < max_test_steps:
     action, _states = model.predict(obs)
     obs, rewards, done, info = env.step(action)
     # grasp fail is mapped to the number 3 in the info dictionary.
-    if done and 3 in info:
-        env.reset()
+    if done:
+        if 1 in info or 3 in info:
+            env.reset()
     env.render()
+
+with open('data/obj_pose_error.data', 'wb') as filehandle:
+    pickle.dump(env.obj_pose_error_data, filehandle)
