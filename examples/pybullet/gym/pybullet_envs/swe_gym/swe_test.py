202aedc14fd781187c604265f96250c249a69da7
Victor-Aladele <valad@gatech.edu>
2022-01-04 10:24:12 +0100

    Second test run.

--- examples/pybullet/gym/pybullet_envs/swe_gym/swe_test.py
+++ examples/pybullet/gym/pybullet_envs/swe_gym/swe_test.py
@@ -19,6 +19,6 @@ while env.time_step < max_test_steps:
     env.render()
 
 folder = "rl"
-exp_run = 2
+exp_run = 3
 with open('data/{}/obj_pose_error_{}.data'.format(folder, exp_run), 'wb') as filehandle:
     pickle.dump(env.obj_pose_error_data, filehandle)
