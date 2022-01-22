b5eea87fe76a080b642601a818c37c69eb186246
Victor-Aladele <valad@gatech.edu>
2022-01-22 11:18:21 -0500

    Reduce the learning rate.

--- examples/pybullet/gym/pybullet_envs/swe_gym/swe_train.py
+++ examples/pybullet/gym/pybullet_envs/swe_gym/swe_train.py
@@ -24,7 +24,7 @@ class CustomPolicy(FeedForwardPolicy):
                                            feature_extraction="mlp")
 
 # model = PPO2(CustomPolicy, env, verbose=1, tensorboard_log="./data/ppo2_coop_manip_tensorboard/")
-model = PPO2(CustomPolicy, env, verbose=1)
+model = PPO2(CustomPolicy, env, learning_rate=2.5e-6, verbose=1)
 # Train the agent
 total_timesteps = 10000
 model.learn(total_timesteps=total_timesteps)
