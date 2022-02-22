531f65f5426b301a91b650ba8b836751db14a9af
Victor-Aladele <valad@gatech.edu>
2022-02-22 14:25:09 -0500

    Trained with seed of 2 for 2M times steps.

--- examples/pybullet/gym/pybullet_envs/swe_gym/swe_train.py
+++ examples/pybullet/gym/pybullet_envs/swe_gym/swe_train.py
@@ -30,7 +30,7 @@ class CustomPolicy(FeedForwardPolicy):
 #                                            feature_extraction="mlp")
 
 # model = PPO2(CustomPolicy, env, verbose=1, learning_rate=2.5e-5, tensorboard_log="./data/ppo2_coop_manip_tensorboard/")
-model = PPO2(CustomPolicy, env, verbose=1, learning_rate=2.5e-6, seed=5)
+model = PPO2(CustomPolicy, env, verbose=1, learning_rate=2.5e-6, seed=2)
 # Train the agent
 total_timesteps = 2000000
 model.learn(total_timesteps=total_timesteps)
